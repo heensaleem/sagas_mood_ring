f831e63ec0439649214ffa11e344d0e6d0037e47
heensaleem <heenakouser440@gmail.com>
2019-05-18 13:16:53 -0500

    Routes working tested with postman

---
+++ server/Routes/ImagesTags.router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const pool = require('../modules/pool');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  // Get all images_tags
+  let query = `SELECT * FROM "images_tags"`;
+  pool.query(query).then( result => {
+    res.send(result.rows);
+  }).catch(err => {
+    console.log(err);
+    res.sendStatus(500);
+  })
+})
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 5000;
+
+//Route includes
+const imagesRouter = require('./routes/images.router');
+const tagsRouter = require('./routes/tags.router');
+const ImagesTagsRouter = require('./routes/ImagesTags.router')
+
+
+/** ---------- MIDDLEWARE ---------- **/
+app.use(bodyParser.json()); // needed for angular requests
+app.use(express.static('build'));
+
+/** ---------- ROUTES ---------- **/
+app.use('/api/images' , imagesRouter);
+app.use('/api/tags', tagsRouter);
+app.use('/api/images/addtag', ImagesTagsRouter)
+
+/** ---------- START SERVER ---------- **/
+app.listen(port, function () {
+    console.log('Listening on port: ', port);
+});
